2965c7daf942ad1246c01ab52dd98c8b012c5c62
audumbar-raj-nevarekar <audumbar.raj.nevarekar@hpe.com>
2016-06-15 17:47:02 +0530

    Added examples of ng-repeat

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,31 @@
 var myApp = angular
 				.module("myModule", [])
 				.controller('myController', function($scope){
-	var employee = {
-			firstName : "Audumbar",
-			lastName : "Nevarekar",
-			gender : "male"
-	};
-	$scope.employee = employee;
+	//Example 1
+	var employees = [
+				{firstName : "Audumbar", lastName : "Nevarekar", gender : "male", salary : "25000"},
+				{firstName : "Anniruddha", lastName : "Lanke", gender : "male", salary : "25000"},
+				{firstName : "Pravin", lastName : "Chavan", gender : "male", salary : "25000"},
+				{firstName : "Rohit", lastName : "Yadav", gender : "male", salary : "25000"},
+				{firstName : "VIshal", lastName : "Bharadwaj", gender : "male", salary : "25000"}
+	];
+	$scope.employees = employees;
+
+	//Example 2: Nested
+	var countries = [
+			{
+				name: "India",
+				cities: [ "Mumbai", "Pune", "Bengaluru" ]
+			},
+			{
+				name: "USA",
+				cities: [ "Washington D.C.", "Texas", "California" ]
+			},
+			{
+				name: "UK",
+				cities: [ "London", "Cardiff", "Birmingham" ]
+			}
+	];
+	$scope.countries = countries;
 });
 				
