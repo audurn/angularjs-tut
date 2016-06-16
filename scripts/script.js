2cd9b859539f382b04d58aba015c355c7ab12cba
audumbar-raj-nevarekar <audumbar.raj.nevarekar@hpe.com>
2016-06-16 19:11:46 +0530

    using ng-hide & ng-show directives

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ var myApp = angular
 				.module("myModule", [])
 				.controller('myController', function($scope){
 	var employees = [
-			{name: "Ben", gender: 1, city: "London" },
-			{name: "Sara", gender: 2, city: "Paris"},
-			{name: "Mark", gender: 1, city: "California" },
-			{name: "Pam", gender: 2, city: "Frankfurt" },
-			{name: "Todd", gender: 3, city: "Silicon Valley" }
+			{name: "Ben", gender: "Male", salary: 55000.72, city: "London" },
+			{name: "Sara", gender: "Female", salary: 61000.45, city: "Paris"},
+			{name: "Mark", gender: "Male", salary: 70000.00, city: "California" },
+			{name: "Pam", gender: "Male", salary: 75000.72, city: "Frankfurt" },
+			{name: "Todd", gender: "Male", salary: 72000.444, city: "Silicon Valley" }
 	];
 	$scope.employees = employees;
 });
