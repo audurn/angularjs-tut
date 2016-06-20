f4868fe2ad581a33538e2d9a494c55320f46a421
audumbar-raj-nevarekar <audumbar.raj.nevarekar@hpe.com>
2016-06-20 18:28:17 +0530

    consuming web service

---
+++ EmployeeService.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Web.Script.Serialization;
+
+namespace ConnectToMysql
+{
+    /// <summary>
+    /// Summary description for EmployeeService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+     [System.Web.Script.Services.ScriptService]
+    public class EmployeeService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void GetAllEmployees()
+        {
+            //<NOTE:IMPORTANT> As you are using Mysql database, you have to use MySqlCOnnection, MySqlCommand,
+            //MySqlDataReader from MySql.Data.MySql Client 
+            List<Employee> listEmployees = new List<Employee>();
+            String cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            MySqlConnection con;
+            using (con = new MySqlConnection(cs))
+            {
+                MySqlCommand cmd = new MySqlCommand("select * from employee", con);
+                con.Open();
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.id = Convert.ToInt32(rdr["id"]);
+                    employee.name = rdr["name"].ToString();
+                    employee.gender = rdr["gender"].ToString();
+                    employee.salary = Convert.ToInt32(rdr["salary"]);
+                    listEmployees.Add(employee);
+                }
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                Context.Response.Write(js.Serialize(listEmployees));
+            }
+        }
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,9 @@
+/// <reference path="angular.js" />
 var myApp = angular
-				.module("myModule", [])
-				.controller('myController', function($scope){
-	var employees = [
-			{name: "Ben", gender: "Male", salary: 55000.72, city: "London" },
-			{name: "Sara", gender: "Female", salary: 61000.45, city: "Paris"},
-			{name: "Mark", gender: "Male", salary: 70000.00, city: "California" },
-			{name: "Pam", gender: "Male", salary: 75000.72, city: "Frankfurt" },
-			{name: "Todd", gender: "Male", salary: 72000.444, city: "Silicon Valley" }
-	];
-	$scope.employees = employees;
-});
-				
+                .module('myModule', [])
+                .controller('myController', function ($scope, $http) {
+                    $http.get('EmployeeService.asmx/GetAllEmployees')
+                    .then(function (response) {
+                        $scope.employees = response.data;
+                    });
+            });
