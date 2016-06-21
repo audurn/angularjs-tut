808055381aa2ad89b4e5ea674704287fe7da0833
audurn <audurn@users.noreply.github.com>
2016-06-21 12:12:40 +0530

    added callback functions
    added more specific success and error callback functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
 /// <reference path="angular.js" />
 var myApp = angular
                 .module('myModule', [])
-                .controller('myController', function ($scope, $http) {
-                    $http.get('EmployeeService.asmx/GetAllEmployees')
-                    .then(function (response) {
+                .controller('myController', function ($scope, $http, $log) {
+                    var successCallback = function (response) {
                         $scope.employees = response.data;
-                    });
+                        $scope.isError = false;
+                    }
+                    var errorCallback = function (reason) {
+                        $scope.error = reason.data;
+                        $scope.isError = true;
+                    }
+                    $http({
+                        method : 'GET',
+                        url: 'EmployeeService.asmx/GetAllEmployees'
+                    })
+                    .then(successCallback, errorCallback);
             });
