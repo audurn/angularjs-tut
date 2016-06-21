1ef937e60fdf1725961da9dcb72de948326ca6eb
audumbar-raj-nevarekar <audumbar.raj.nevarekar@hpe.com>
2016-06-21 16:16:53 +0530

    dont know what i am doing

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySQLDemo
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public String gender { get; set; }
+        public int salary { get; set; }
+    }
+}
