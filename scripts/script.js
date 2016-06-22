da924c5a0e6b9d50babd1fbff91e2dea9d91ce96
audumbar-raj-nevarekar <audumbar.raj.nevarekar@hpe.com>
2016-06-22 12:52:09 +0530

    example of anchorscroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,10 @@
 var myApp = angular
 				.module("myModule", [])
-				.controller('myController', function($scope){
-	$scope.transformString = function(input) {
-		if (!input) {
-			return input;
-		}
-
-		var output = "";
-
-		for (var i = 0; i < input.length; i++) {
-			if (i>0 && input[i]==input[i].toUpperCase()) {
-				output = output + " ";
-			}
-
-			output = output + input[i];
-		}
-		$scope.output = output;
-		
-	}
+				.controller('myController', function($scope, $location, $anchorScroll){
+		$scope.scrollTo = function (scrollLocation) {
+                             $location.hash(scrollLocation);
+                             $anchorScroll.yOffset = 20;
+                             $anchorScroll();
+                         }
 });
 				
